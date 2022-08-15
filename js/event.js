a8fa78932f05be2274bc402834251957c8f8b18f
ea-watanabe <102790979+ea-watanabe@users.noreply.github.com>
2022-08-16 08:52:28 +0900

    Update event.js

--- js/event.js
+++ js/event.js
@@ -1,7 +1,2 @@
 let select = document.getElementsByClassName('stt-select')
-select.addEventListener('change',textGet,false);
-
-function textGet(){
-   console.log('取得しました')
-}
-
+select.onchange = function() { console.log('取得しました')}
