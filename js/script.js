162b97963d524fd40b701cec0d77256a36bf71f1
ea-watanabe <102790979+ea-watanabe@users.noreply.github.com>
2022-08-16 08:08:53 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-$(function(){$("nav div.panel").hide();$(".menu").click(function(){$(this).toggleClass("menuOpen").next().slideToggle();});})
+$(function(){$("nav div.panel").hide();$(".menu").click(function(){$(this).toggleClass("menuOpen").next().slideToggle();}
+                                                       let select = document.querySelector('[class="stt-select"]');
+
+select.onchange = event => { 
+  console.log(select.selectedIndex);
+｝
+                                                       );})
