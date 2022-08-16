70a72248f390b20972d6c28f1f063ba9c101ad64
ea-watanabe <102790979+ea-watanabe@users.noreply.github.com>
2022-08-16 09:26:26 +0900

    Update event.js

--- js/event.js
+++ js/event.js
@@ -1,6 +1,3 @@
-#let select = document.getElementsByClassName('stt-select')
-#select.onclick = function() { console.log('取得しました')}
-
 document.getElementById('stt-select').addEventListener("click", function(e){
   let elem = e.target;
   if(elem.classList.contains("stt-item")){
