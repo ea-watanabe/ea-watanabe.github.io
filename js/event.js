b0f3af736246b1130edac411a4133f3c2dabb7f7
ea-watanabe <102790979+ea-watanabe@users.noreply.github.com>
2022-08-16 09:18:25 +0900

    Update event.js

--- js/event.js
+++ js/event.js
@@ -1,3 +1,2 @@
 let select = document.getElementsByClassName('stt-item')
-select[0].onclick = function() { console.log('取得しました')}
-select[1].onclick = function() { console.log('取得しました')}
+select.onclick = function() { console.log('取得しました')}
